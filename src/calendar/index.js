572b8122ad0b0253710bb7e1f22102eb54005549
kLe0h <elmastrollxd34@gmail.com>
2024-05-20 00:20:11 -0600

    Agregando eventos de calendario y vistas por defecto

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -1,3 +1,5 @@
 export * from './pages/CalendarPage'
 
 export * from './components/NavBar'
+
+export * from './components/CalendarEvent'
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,15 +1,16 @@
 import { Calendar } from 'react-big-calendar'
 import { addHours } from 'date-fns'
-import { NavBar } from '../'
+import { CalendarEvent, NavBar } from '../'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { localizer, getMessagesES } from '../../helpers'
+import { useState } from 'react'
 
 
 const events = [{
   title: 'Nuevo titulo',
   notes: 'Estas son las notas',
   start: new Date(),
-  end: addHours(new Date(), 0.2), 
+  end: addHours(new Date(), 2), 
   bgColor: '#fafafa',
   user: {
     _id: '123',
@@ -20,6 +21,34 @@ const events = [{
 
 
 export const CalendarPage = () => {
+  
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month')
+  
+  const onDoubleClick = (e) => {
+  
+  }
+
+  const onSelect = (e) => {
+
+  }
+
+  const onViewChange = (e) => {
+    localStorage.setItem('lastView', e) // Guardar en el localStorage
+  }
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block',
+      color: 'white',
+    }
+    return {
+      style
+    }
+  }
+  
   return (
     <>
       <NavBar />
@@ -30,9 +59,17 @@ export const CalendarPage = () => {
           events={events}
           startAccessor="start"
           endAccessor="end"
+          defaultView={lastView}
           style={{ height: 500 }}
           culture='es'
-          messages={ getMessagesES() }          
+          messages={ getMessagesES() }  
+          eventPropGetter={ eventStyleGetter } 
+          components={{
+            event: CalendarEvent
+          }}       
+          onDoubleClickEvent={ onDoubleClick }
+          onSelectEvent={ onSelect }
+          onView={ onViewChange }
         />
       </div>
     </>
